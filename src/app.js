0a0e42cda94832b60687b55ad22a06474b55f520
Joseph Niati <emcmonkey12@gmail.com>
2022-07-25 23:26:48 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,21 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let generateDomain = () => {
+    document.querySelector("#btn").addEventListener("click", () => {});
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let n = 0; n < noun.length; n++) {
+          console.log(pronoun[i] + adj[j] + noun[n] + ".com");
+        }
+      }
+    }
+  };
+  // let myArr = generateDomain();
+  // console.log(generateDomain());
+  generateDomain();
 };
